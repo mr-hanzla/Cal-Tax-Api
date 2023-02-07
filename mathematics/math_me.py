6cdc85a2aa3e0d69d1417e682799b44d19d4fcc6
Mr Hanzla <mr.hanzla101@gmail.com>
2023-02-07 23:14:10 +0500

    add api method for quadratic formula

--- mathematics/math_me.py
+++ mathematics/math_me.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, abort
+from flask import Blueprint, abort, request
 from jinja2 import TemplateNotFound
 
 math_me_bp = Blueprint('math_me', __name__,url_prefix='/math')
@@ -8,3 +8,16 @@ math_me_bp = Blueprint('math_me', __name__,url_prefix='/math')
 def show():
     return "Yayyyy, bluepring is working fine!"
 
+
+@math_me_bp.route('/formula/quadratic')
+def quadratic_formula():
+    a = b = c = 0
+    a = 5
+    try:
+        a = request.args['a']
+        b = request.args['b']
+        c = request.args['c']
+    except:
+        return abort(404, 'Yaaaar!, bongia na mara karo! [a,b,c] ki values pass karo')
+
+    return 'OK'
