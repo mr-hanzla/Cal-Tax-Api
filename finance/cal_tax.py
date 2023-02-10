769a05597e2db1845f79510eaa44b188b529a1e7
Mr Hanzla <mr.hanzla101@gmail.com>
2023-02-10 10:45:00 +0500

    render template for cal_tax index route & move template file to template dir

--- finance/cal_tax.py
+++ finance/cal_tax.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
+from Util import Util
 
 cal_tax = Blueprint('cal_tax', __name__, url_prefix='/tax')
 
-cal_tax.route('/')
+@cal_tax.route('/')
 def tax_index():
-    return render_template('tax_index.html')
+    return render_template('finance/tax_index.html')
